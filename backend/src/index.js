892d276a88a026d5fac5c4cd1a5981957c6da1e6
LepharamRamchiary <lepharamchiary@gmail.com>
2025-04-30 10:18:54 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
